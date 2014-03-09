485388b6e5ede10e9ca993053bf673c138ec6aea
shiraz <q.shiraz@gmail.com>
2014-03-09 07:05:46 -0400

    building wifi

--- src/com/hackathon/collisionavoidancewarning/MainActivity.java
+++ src/com/hackathon/collisionavoidancewarning/MainActivity.java
@@ -1,7 +1,11 @@
 package com.hackathon.collisionavoidancewarning;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.net.wifi.p2p.WifiP2pManager.Channel;
+import android.os.Bundle;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -10,6 +14,8 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		setupWifi();
 	}
 
 	@Override
@@ -18,5 +24,31 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	private void setupWifi() {
+		//  Indicates a change in the Wi-Fi P2P status.
+		mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+
+	    // Indicates a change in the list of available peers.
+	    mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+
+	    // Indicates the state of Wi-Fi P2P connectivity has changed.
+	    mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+
+	    // Indicates this device's details have changed.
+	    mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+	    
+	    mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+	    mChannel = mManager.initialize(this, getMainLooper(), null);
+	}
+	
+	
+	/* instance variables */
+	private final IntentFilter mIntentFilter = new IntentFilter();
+	private Channel mChannel;
+	private WifiP2pManager mManager;
+	
+	/* class variables */
+	public static boolean wifiEnabled = false;
 
 }
---
+++ src/com/hackathon/collisionavoidancewarning/WifiBroadcastReceiver.java
@@ -0,0 +1,46 @@
+package com.hackathon.collisionavoidancewarning;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pManager;
+
+public class WifiBroadcastReceiver extends BroadcastReceiver {
+
+	public WifiBroadcastReceiver() {
+		// TODO Auto-generated constructor stub
+	}
+
+	 @Override
+	    public void onReceive(Context context, Intent intent) {
+	        String action = intent.getAction();
+	        if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+	            // Determine if Wifi P2P mode is enabled or not, alert
+	            // the Activity.
+	            int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
+	            if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
+	                MainActivity.wifiEnabled = true;
+	            } else {
+	                MainActivity.wifiEnabled = false;
+	            }
+	        } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+
+	            // The peer list has changed!  We should probably do something about
+	            // that.
+
+	        } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
+
+	            // Connection state changed!  We should probably do something about
+	            // that.
+
+	        } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
+//	            DeviceListFragment fragment = (DeviceListFragment) activity.getFragmentManager()
+//	                    .findFragmentById(R.id.frag_list);
+//	            fragment.updateThisDevice((WifiP2pDevice) intent.getParcelableExtra(
+//	                    WifiP2pManager.EXTRA_WIFI_P2P_DEVICE));
+
+	        }
+	    }
+
+}
